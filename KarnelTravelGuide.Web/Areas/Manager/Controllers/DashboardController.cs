7026392b471875c59ea2b315886cd1741fe3993d
tan050506 <lvmtana24098@cusc.ctu.edu.vn>
2026-03-31 10:56:30 +0700

    đổi đăng nhập đăng ký sang tiếng anh và làm lại dashboard cho manager

--- KarnelTravelGuide.Web/Areas/Manager/Controllers/DashboardController.cs
+++ KarnelTravelGuide.Web/Areas/Manager/Controllers/DashboardController.cs
@@ -1,12 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KarnelTravelGuide.Web.Data;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace KarnelTravelGuide.Web.Areas.Manager.Controllers
 {
-    [Area("Manager")] 
+    [Area("Manager")]
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // 1. Đếm hóa đơn chưa thanh toán (Unpaid)
+            ViewBag.PendingInvoicesCount = await _context.Invoices
+                .CountAsync(i => i.PaymentStatus == "Unpaid");
+
+            // 2. Đếm tổng số tuyến đường/phương tiện (Transportation)
+            ViewBag.ActiveRoutesCount = await _context.Transportations.CountAsync();
+
+            // 3. Đếm số lượng phòng đã được xác nhận (Confirmed)
+            ViewBag.RoomsBookedCount = await _context.OrderDetails
+                .Include(od => od.Order)
+                .CountAsync(od => od.RoomBookingId != null && od.Order!.Status == "Confirmed");
+
+            // 4. MỚI: Đếm số lượng đặt bàn đã được xác nhận (Confirmed)
+            ViewBag.TablesBookedCount = await _context.OrderDetails
+                .Include(od => od.Order)
+                .CountAsync(od => od.ResBookingId != null && od.Order!.Status == "Confirmed");
+
+            // 5. Đếm tổng số feedback của khách hàng
+            ViewBag.FeedbackCount = await _context.Feedbacks.CountAsync();
+
             return View();
         }
     }
